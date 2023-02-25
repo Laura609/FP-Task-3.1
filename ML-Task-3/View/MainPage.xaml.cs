1b82b074a4ce5fe2fccf092bf814318d63682c24
Laura <laura.metnietse@gmail.com>
2023-02-25 15:16:19 +0300

    FP-Task-3.1

--- ML-Task-3/View/MainPage.xaml.cs
+++ ML-Task-3/View/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using ML_Task_3.View.Pages.TaskPages;
 using System;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -16,59 +17,9 @@ namespace ML_Task_3.View
             InitializeComponent();
         }
 
-        private void Button_Click1(object sender, System.Windows.RoutedEventArgs e)
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            CoreNavigate.MyCore?.Navigate(new Task1Page());
-        }
-
-        private void Button_Click2(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task2Page());
-        }
-
-        private void Button_Click3(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task3Page());
-        }
-
-        private void Button_Click4(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task4Page());
-        }
-
-        private void Button_Click5(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task5Page());
-        }
-
-        private void Button_Click6(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task6Page());
-        }
-
-        private void Button_Click7(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task7Page());
-        }
-
-        private void Button_Click8(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task8Page());
-        }
-
-        private void Button_Click9(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task9Page());
-        }
-
-        private void Button_Click10(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task10Page());
-        }
-
-        private void Button_Click11(object sender, System.Windows.RoutedEventArgs e)
-        {
-            CoreNavigate.MyCore?.Navigate(new Task11Page());
+            Application.Current.Shutdown();
         }
     }
 }
--- ML-Task-3/View/Pages/TaskPages/Task1Page.xaml.cs
+++ ML-Task-3/View/Pages/TaskPages/Task1Page.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ML_Task_3.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace ML_Task_3.View.Pages.TaskPages
         public Task1Page()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyCore?.GoBack();
         }
     }
 }
